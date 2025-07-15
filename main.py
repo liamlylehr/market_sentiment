04d3ace4ac90f9e93a683eecc9e2730a558fda06
Liam Y Lehr <liamlylehr@gmail.com>
2025-07-14 21:35:33 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import feedparser
+
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+ticker = 'AAPL'
+keyword = 'Apple'
+
+pipe = pipeline("text-classification", model="ProsusAI/finbert")
+
+rss_url = f'https://finance.yahoo.com/rss/headline?s={ticker}'
+
+feed = feedparser.parse(rss_url)
+
+total_score = 0
+num_articles = 0
+
+for i, entry in enumerate(feed.entries):
+    if keyword.lower() not in entry.summary.lower():
+        continue
+    print(f'Title: {entry.title}')
+    print(f'Summary: {entry.summary}')
+    print(f'Link: {entry.link}')
+    print(f'Published: {entry.published}')
+
+    sentiment = pipe(entry.summary)[0]
+    print(f'Sentiment: {sentiment["label"]} (Score: {sentiment["score"]:.4f})\n')
+    print('-' * 80)
+
+    if sentiment['label'] == 'positive':
+        total_score += sentiment['score']
+        num_articles += 1
+    elif sentiment['label'] == 'negative':
+        total_score -= sentiment['score']
+        num_articles += 1
+
+final_score = total_score / num_articles if num_articles > 0 else 0
+print(f'Sentiment for {ticker}: {"Positive" if total_score > 0.15 else "Negative" if total_score < 0.15 else "Neutral"} {final_score:.4f} after {num_articles} articles')
