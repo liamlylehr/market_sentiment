833faadefbfe984b13d8a89679589331bce5a62f
Liam Y Lehr <liamlylehr@gmail.com>
2025-07-14 21:58:51 -0500

    few changes

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import feedparser
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 
-ticker = 'AAPL'
-keyword = 'Apple'
+ticker = 'AVGO'
+keyword = 'BROADCOM'
 
 pipe = pipeline("text-classification", model="ProsusAI/finbert")
 
@@ -15,17 +15,18 @@ feed = feedparser.parse(rss_url)
 total_score = 0
 num_articles = 0
 
+print(f'\nAnalyzing sentiment for {ticker} ({keyword})')
 for i, entry in enumerate(feed.entries):
     if keyword.lower() not in entry.summary.lower():
         continue
+    print('-' * 80)
     print(f'Title: {entry.title}')
     print(f'Summary: {entry.summary}')
     print(f'Link: {entry.link}')
     print(f'Published: {entry.published}')
 
     sentiment = pipe(entry.summary)[0]
-    print(f'Sentiment: {sentiment["label"]} (Score: {sentiment["score"]:.4f})\n')
-    print('-' * 80)
+    print(f'Sentiment: {sentiment["label"]} (Score: {sentiment["score"]:.4f})')
 
     if sentiment['label'] == 'positive':
         total_score += sentiment['score']
@@ -35,4 +36,7 @@ for i, entry in enumerate(feed.entries):
         num_articles += 1
 
 final_score = total_score / num_articles if num_articles > 0 else 0
-print(f'Sentiment for {ticker}: {"Positive" if total_score > 0.15 else "Negative" if total_score < 0.15 else "Neutral"} {final_score:.4f} after {num_articles} articles')
+print()
+print('=' * 80)
+print(f'FINAL SCORE - Sentiment for {ticker}: {"Positive" if final_score > 0.15 else "Negative" if final_score < 0.15 else "Neutral"} {final_score:.4f} after {num_articles} articles')
+print('=' * 80,'\n')
